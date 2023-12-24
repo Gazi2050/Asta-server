3c918988097ea03209ca019ec15a9ca7b51f4ca5
Gazi2050 <gazi45344@gmail.com>
2023-12-25 00:28:37 +0600

    update server

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://asta-server-three.vercel.app']
+}));
 app.use(express.json());
 
 
@@ -24,13 +26,61 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        //Collections
+        const userCollection = client.db('asta').collection('users');
+        const eventCollection = client.db('asta').collection('events');
+
+        // user related api
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exist', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+
+        //events related api
+        app.get('/events', async (req, res) => {
+            const cursor = eventCollection.find();
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
@@ -38,8 +88,15 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send(`<h1 style="text-align:center">Asta Server Is Running...</h1>`)
+    res.send(`<h1 style="text-align:center">Asta Server Is Running...</h1>
+    <h2 style="text-align:center;font-family:Monospace;"><a href='http://localhost:5000/users'>users</a></h2>
+    <h2 style="text-align:center;font-family:Monospace;"><a href='http://localhost:5000/events'>events</a></h2>`)
 })
+// app.get('/', (req, res) => {
+//     res.send(`<h1 style="text-align:center">Asta Server Is Running...</h1>
+//     <h2 style="text-align:center;font-family:Monospace;"><a href='https://asta-server-three.vercel.app/users'>users</a></h2>
+//     <h2 style="text-align:center;font-family:Monospace;"><a href='https://asta-server-three.vercel.app/events'>events</a></h2>`)
+// })
 
 app.listen(port, () => {
     console.log(`Asta Is Running On Port: ${port}`)
